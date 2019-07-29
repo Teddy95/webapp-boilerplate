b9a6d826d4b389325e0d8e1088febbb8283e8841
Andre <webmaster@andre-sieverding.de>
2019-07-29 10:15:37 +0200

    Remove electron

--- gulpfile.js
+++ gulpfile.js
@@ -52,19 +52,3 @@ gulp.task('watch', done => {
  */
 gulp.task('compile', gulp.parallel('compile:sass', 'compile:app'))
 gulp.task('compile:dev', gulp.parallel('compile:sass', 'compile:app:watch'))
-
-/**
- * Package App to /release
- */
-if (process.platform === 'win32') {
-	var packageInfo = JSON.parse(fs.readFileSync('./package.json'))
-	gulp.task('package:mac', shell.task('electron-packager . --overwrite --platform=darwin --arch=x64 --icon=' + packageInfo.build.icon.mac + ' --prune=true --out=release'))
-	gulp.task('package:windows', shell.task('electron-packager . ' + packageInfo.name + ' --overwrite --asar=true --platform=win32 --arch=ia32 --icon=' + packageInfo.build.icon.windows + ' --prune=true --out=release --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName=' + packageInfo.productName))
-	gulp.task('package:linux', shell.task('electron-packager . ' + packageInfo.name + ' --overwrite --asar=true --platform=linux --arch=x64 --icon=' + packageInfo.build.icon.linux + ' --prune=true --out=release'))
-} else {
-	gulp.task('package:mac', shell.task('electron-packager . --overwrite --platform=darwin --arch=x64 --icon=$npm_package_build_icon_mac --prune=true --out=release'))
-	gulp.task('package:windows', shell.task('electron-packager . $npm_package_name --overwrite --asar=true --platform=win32 --arch=ia32 --icon=$npm_package_build_icon_windows --prune=true --out=release --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName=$npm_package_productName'))
-	gulp.task('package:linux', shell.task('electron-packager . $npm_package_name --overwrite --asar=true --platform=linux --arch=x64 --icon=$npm_package_build_icon_linux --prune=true --out=release'))
-}
-
-gulp.task('release', gulp.series('compile:sass', gulp.parallel('package:mac', 'package:windows', 'package:linux')))
