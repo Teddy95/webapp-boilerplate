33421aa0ff5fb6478511162b89feb1f6ede00961
Andre <59558264+asieverding@users.noreply.github.com>
2020-05-26 12:57:08 +0200

    Use browser lang as default locale

--- app/scripts/i18n.js
+++ app/scripts/i18n.js
@@ -2,7 +2,7 @@ import i18nForBrowser, { __, __n } from 'i18n-for-browser'
 import locales_de from '../locales/de.json'
 import locales_en from '../locales/en.json'
 
-i18nForBrowser.configure({
+let i18nOptions = {
 	locales: {
 		'de': locales_de,
 		'en': locales_en
@@ -10,7 +10,17 @@ i18nForBrowser.configure({
 	defaultLocale: 'en',
 	queryParameter: 'lang',
 	cookieName: 'i18n'
-})
+}
+
+if (typeof window !== 'undefined') {
+	let browserLanguage = navigator.language || navigator.userLanguage
+
+	if (typeof i18nOptions.locales[browserLanguage.toLowerCase()] !== 'undefined') {
+		i18nOptions.defaultLocale = browserLanguage.toLowerCase()
+	}
+}
+
+i18nForBrowser.configure(i18nOptions)
 
 if (typeof window === 'undefined') {
 	global.__ = __
